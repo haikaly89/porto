6209395fefb286d15024603f9b604a426576fd9d
haikaly89 <haikalyusuf89@gmail.com>
2026-01-20 11:58:32 +0700

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
 
 import markdoc from '@astrojs/markdoc';
 
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  integrations: [tailwind(), react(), keystatic(), markdoc()],
+  integrations: [tailwind(), react(), markdoc()],
   output: 'static',
   adapter: vercel(),
 });
